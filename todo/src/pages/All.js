4d193e90e73fe5e95b8bbf9672826b5b581b6e43
João Costa <costav1402@gmail.com>
2021-08-24 16:21:52 -0300

    implementing line-through

--- todo/src/pages/All.js
+++ todo/src/pages/All.js
@@ -21,7 +21,7 @@ export default function PageAll({todos, setTodos, setEditTodo}) {
         <ul className="todo-todo__list">
         {todos.map((todo) =>
         <li className="todo-todo__list-item" key={todo.id}>
-            <span className="todo-todo__list-item-title">{todo.title}</span>
+            <span className={`todo-todo__list-item-title ${todo.isDone? 'line-through' : ''}`}>{todo.title}</span>
         <div className="todo-todo__list-item-btns">
             <button onClick={()=>handleComplete(todo)}>{todo.isDone? (<i className="fas fa-check-square"></i>) : (<i className="far fa-check-square"></i>)}</button>
             <button onClick={()=>handleEdit(todo)}><i className="far fa-edit"></i></button>
--- todo/src/pages/Completed.js
+++ todo/src/pages/Completed.js
@@ -21,7 +21,7 @@ export default function PageCompleted({todos, setTodos, setEditTodo, completeds,
         <ul className="todo-todo__list">
         {completeds.map(completed=>
             <li className="todo-todo__list-item" key={completed.title}>
-                <span className="todo-todo__list-item-title">{completed.title}</span>
+                <span className="todo-todo__list-item-title line-through">{completed.title}</span>
             <div className="todo-todo__list-item-btns">
             <button onClick={()=>handleComplete(completed)}>{completed.isDone? (<i className="fas fa-check-square"></i>) : (<i className="far fa-check-square"></i>)}</button>
             <button onClick={()=>handleEdit(completed)}><i className="far fa-edit"></i></button>
